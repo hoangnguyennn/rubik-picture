9550b1b656582f28f4e21a21678d8c4905cc44d9
hoangnguyennn <nvanhoang188@gmail.com>
2020-11-15 12:31:31 +0700

    format with prettier

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.addEventListener('mousemove', (e) => {
-	var rotate_X;
-	var rotate_Y;
+  var rotate_X;
+  var rotate_Y;
 
-	rotate_X = e.pageX;
-	rotate_Y = -e.pageY;
+  rotate_X = e.pageX;
+  rotate_Y = -e.pageY;
 
-	document.querySelector('.container').style.transform = 
-		`rotateX(${rotate_Y}deg) rotateY(${rotate_X}deg)`;
+  document.querySelector(
+    '.container'
+  ).style.transform = `rotateX(${rotate_Y}deg) rotateY(${rotate_X}deg)`;
 });
